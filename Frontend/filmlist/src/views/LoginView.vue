2993216431a1f43a8620cb9e265860645ffc4118
Aurel Bieri <bieri.aurel@gmail.com>
2024-09-07 20:27:58 +0200

    Ich habe eine main.css Datei erstellt und in der Login und App vue Testcode eingefügt.

--- Frontend/filmlist/src/views/LoginView.vue
+++ Frontend/filmlist/src/views/LoginView.vue
@@ -1,4 +1,12 @@
 <script setup>
-import { useRouter } from 'vue-router'
-import { ref } from 'vue';
+
 </script>
+
+<template>
+    <div class="screen-center" id="login-view">
+        <p>hello</p>
+        <div class="container container--narrower">
+            <p>Hello</p>
+        </div>
+    </div>
+</template>
