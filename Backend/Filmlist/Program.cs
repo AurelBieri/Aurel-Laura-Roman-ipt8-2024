01f4d595a211dbf9bd1356950d86fa14b00b7aae
Aurel Bieri <bieri.aurel@gmail.com>
2024-09-12 15:59:08 +0200

    Ich habe ein c# backend Projekt eingerichtet.

---
+++ Backend/Filmlist/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DatabaseConnectionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=; Port=3308; Database=filmlist;User Id=demo;Password=demo;";
+
+            string query = "SELECT TOP 5 * FROM YourTable";
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection Open!");
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine(reader[0].ToString());
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine("Connection Closed.");
+                }
+            }
+        }
+    }
+}
